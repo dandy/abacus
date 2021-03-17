1bae6ed0fc94b4856ab4de58d61dbb90a4ed35b8
Jessie Ross <jessie.ross@automattic.com>
2021-03-17 12:58:21 +0800

    Add Experiment Health Utils (#473)
    * Implement AggregateRecommendation
    
    * Fix behaviour for missing variationId
    
    * Fix casing
    
    * Use defaultStrategy behaviour
    
    * Improve tests
    
    * Rename AggregateRecommendationType to Decision
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Update src/components/experiments/single-view/results/AggregateRecommendationDisplay.test.tsx
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>
    
    * Reafactor recommendation code
    
    * Fix code
    
    * Update snapshots
    
    * Fix debug component
    
    * Add top level stats
    
    * Simplify things
    
    * Format the code
    
    * Add binomialProbValue
    
    * Add getParticipantCounts
    
    * Implement getExperimentHealthStats
    
    * Implement getExperimentHealthStats
    
    * Fix formatting
    
    * Fix styles
    
    * Have binomialProbValue take an object
    
    * Clean up
    
    * Add references
    
    * Improve documentation
    
    * Move the abs
    
    * Use stdlib for the binomial test
    
    * Fix formatting
    
    * Clean up
    
    * Clean up JSDoc
    
    * Fix formatting
    
    Co-authored-by: Yanir Seroussi <yanir.seroussi@automattic.com>

--- src/lib/analyses.test.ts
+++ src/lib/analyses.test.ts
@@ -304,3 +304,205 @@ describe('getAggregateRecommendation', () => {
     })
   })
 })
+
+describe('getParticipantCounts', () => {
+  it('should work correctly', () => {
+    expect(
+      Analyses.getParticipantCounts(
+        Fixtures.createExperimentFull({
+          variations: [
+            { variationId: 1, allocatedPercentage: 50, isDefault: true, name: 'variation_name_1' },
+            { variationId: 2, allocatedPercentage: 50, isDefault: false, name: 'variation_name_2' },
+          ],
+        }),
+        {
+          [AnalysisStrategy.IttPure]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 100,
+              variation_1: 40,
+              variation_2: 70,
+            },
+          }),
+          [AnalysisStrategy.MittNoCrossovers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 90,
+              variation_1: 35,
+              variation_2: 55,
+            },
+          }),
+          [AnalysisStrategy.MittNoSpammers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 85,
+              variation_1: 40,
+              variation_2: 45,
+            },
+          }),
+          [AnalysisStrategy.MittNoSpammersNoCrossovers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 60,
+              variation_1: 25,
+              variation_2: 35,
+            },
+          }),
+          [AnalysisStrategy.PpNaive]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 40,
+              variation_1: 15,
+              variation_2: 25,
+            },
+          }),
+        },
+      ),
+    ).toEqual({
+      total: {
+        assigned: 100,
+        assignedCrossovers: 10,
+        assignedSpammers: 15,
+        exposed: 40,
+      },
+      byVariationId: {
+        '1': {
+          assigned: 40,
+          assignedCrossovers: 5,
+          assignedSpammers: 0,
+          exposed: 15,
+        },
+        '2': {
+          assigned: 70,
+          assignedCrossovers: 15,
+          assignedSpammers: 25,
+          exposed: 25,
+        },
+      },
+    })
+  })
+
+  it('should work correctly without any analyses', () => {
+    expect(
+      Analyses.getParticipantCounts(
+        Fixtures.createExperimentFull({
+          variations: [
+            { variationId: 1, allocatedPercentage: 50, isDefault: true, name: 'variation_name_1' },
+            { variationId: 2, allocatedPercentage: 50, isDefault: false, name: 'variation_name_2' },
+          ],
+        }),
+        {},
+      ),
+    ).toEqual({
+      total: {
+        assigned: 0,
+        assignedCrossovers: 0,
+        assignedSpammers: 0,
+        exposed: 0,
+      },
+      byVariationId: {
+        '1': {
+          assigned: 0,
+          assignedCrossovers: 0,
+          assignedSpammers: 0,
+          exposed: 0,
+        },
+        '2': {
+          assigned: 0,
+          assignedCrossovers: 0,
+          assignedSpammers: 0,
+          exposed: 0,
+        },
+      },
+    })
+  })
+})
+
+describe('getExperimentHealthStats', () => {
+  it('should work correctly', () => {
+    expect(
+      Analyses.getExperimentHealthStats(
+        Fixtures.createExperimentFull({
+          variations: [
+            { variationId: 1, allocatedPercentage: 50, isDefault: true, name: 'variation_name_1' },
+            { variationId: 2, allocatedPercentage: 50, isDefault: false, name: 'variation_name_2' },
+          ],
+        }),
+        {
+          [AnalysisStrategy.IttPure]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 130,
+              variation_1: 40,
+              variation_2: 70,
+            },
+          }),
+          [AnalysisStrategy.MittNoCrossovers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 90,
+              variation_1: 35,
+              variation_2: 55,
+            },
+          }),
+          [AnalysisStrategy.MittNoSpammers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 85,
+              variation_1: 40,
+              variation_2: 45,
+            },
+          }),
+          [AnalysisStrategy.MittNoSpammersNoCrossovers]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 60,
+              variation_1: 25,
+              variation_2: 35,
+            },
+          }),
+          [AnalysisStrategy.PpNaive]: Fixtures.createAnalysis({
+            participantStats: {
+              total: 40,
+              variation_1: 15,
+              variation_2: 27,
+            },
+          }),
+        },
+      ),
+    ).toEqual({
+      probabilities: {
+        byVariationId: {
+          '1': {
+            assignedDistributionMatchingAllocated: 0.000013715068445169529,
+            assignedSpammersDistributionMatchingAllocated: 5.684341886080802e-14,
+            exposedDistributionMatchingAllocated: 0.1538599441628321,
+          },
+          '2': {
+            assignedDistributionMatchingAllocated: 0.43002088397534255,
+            assignedSpammersDistributionMatchingAllocated: 0.5514843298025198,
+            exposedDistributionMatchingAllocated: 0.03847730828420026,
+          },
+        },
+      },
+      ratios: {
+        overall: {
+          assignedCrossoversToAssigned: 0.3076923076923077,
+          assignedSpammersToAssigned: 0.34615384615384615,
+          exposedToAssigned: 0.3076923076923077,
+        },
+        byVariationId: {
+          '1': {
+            assignedCrossoversToAssigned: 0.125,
+            assignedCrossoversToTotalAssignedCrossovers: 0.125,
+            assignedSpammersToAssigned: 0,
+            assignedSpammersToTotalAssignedSpammers: 0,
+            assignedToTotalAssigned: 0.3076923076923077,
+            exposedToAssigned: 0.375,
+            exposedToTotalExposed: 0.375,
+          },
+          '2': {
+            assignedCrossoversToAssigned: 0.21428571428571427,
+            assignedCrossoversToTotalAssignedCrossovers: 0.375,
+            assignedSpammersToAssigned: 0.35714285714285715,
+            assignedSpammersToTotalAssignedSpammers: 0.5555555555555556,
+            assignedToTotalAssigned: 0.5384615384615384,
+            exposedToAssigned: 0.38571428571428573,
+            exposedToTotalExposed: 0.675,
+          },
+        },
+      },
+    })
+  })
+})
--- src/lib/analyses.ts
+++ src/lib/analyses.ts
@@ -1,4 +1,14 @@
-import { Analysis, AnalysisStrategy, RecommendationWarning } from './schemas'
+import _ from 'lodash'
+
+import { Analysis, AnalysisStrategy, ExperimentFull, RecommendationWarning } from './schemas'
+
+// I can't get stdlib to work as an import...:
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const binomialTest = require('@stdlib/stats/binomial-test') as (
+  x: number,
+  n: number,
+  args: { p: number },
+) => { pValue: number }
 
 /**
  * Mapping from AnalysisStrategy to human-friendly descriptions.
@@ -35,6 +45,7 @@ export interface AggregateRecommendation {
 
 /**
  * Returns the aggregate recommendation over analyses of different analysis strategies.
+ *
  * @param analyses Analyses of different strategies for the same day.
  * @param defaultStrategy Default strategy in the context of an aggregateRecommendation..
  */
@@ -75,5 +86,154 @@ export function getAggregateRecommendation(
   return {
     decision: AggregateRecommendationDecision.DeployChosenVariation,
     chosenVariationId: recommendation.chosenVariationId,
+  }
+}
+
+interface AnalysesByStrategy {
+  [AnalysisStrategy.IttPure]?: Analysis
+  [AnalysisStrategy.MittNoCrossovers]?: Analysis
+  [AnalysisStrategy.MittNoSpammers]?: Analysis
+  [AnalysisStrategy.MittNoSpammersNoCrossovers]?: Analysis
+  [AnalysisStrategy.PpNaive]?: Analysis
+}
+
+interface CountsSet {
+  assigned: number
+  assignedCrossovers: number
+  assignedSpammers: number
+  exposed: number
+}
+
+function getParticipantCountsSetForParticipantStatsKey(
+  participantStatsKey: string,
+  analysesByStrategy: AnalysesByStrategy,
+): CountsSet {
+  const assigned = analysesByStrategy[AnalysisStrategy.IttPure]?.participantStats[participantStatsKey] ?? 0
+  return {
+    assigned: assigned,
+    assignedCrossovers:
+      assigned - (analysesByStrategy[AnalysisStrategy.MittNoCrossovers]?.participantStats[participantStatsKey] ?? 0),
+    assignedSpammers:
+      assigned - (analysesByStrategy[AnalysisStrategy.MittNoSpammers]?.participantStats[participantStatsKey] ?? 0),
+    exposed: analysesByStrategy[AnalysisStrategy.PpNaive]?.participantStats[participantStatsKey] ?? 0,
+  }
+}
+
+/**
+ * Gets participant counts for an Experiment
+ */
+export function getParticipantCounts(
+  experiment: ExperimentFull,
+  analysesByStrategy: AnalysesByStrategy,
+): { total: CountsSet; byVariationId: Record<number, CountsSet> } {
+  return {
+    total: getParticipantCountsSetForParticipantStatsKey('total', analysesByStrategy),
+    byVariationId: Object.fromEntries(
+      experiment.variations.map(({ variationId }) => [
+        variationId,
+        getParticipantCountsSetForParticipantStatsKey(`variation_${variationId}`, analysesByStrategy),
+      ]),
+    ),
+  }
+}
+
+interface VariationRatios {
+  exposedToAssigned: number
+  assignedSpammersToAssigned: number
+  assignedCrossoversToAssigned: number
+  exposedToTotalExposed: number
+  assignedToTotalAssigned: number
+  assignedSpammersToTotalAssignedSpammers: number
+  assignedCrossoversToTotalAssignedCrossovers: number
+}
+
+interface VariationProbabilites {
+  exposedDistributionMatchingAllocated: number
+  assignedDistributionMatchingAllocated: number
+  assignedSpammersDistributionMatchingAllocated: number
+}
+
+/**
+ * Gets Experiment Health Stats for an experiment
+ */
+export function getExperimentHealthStats(
+  experiment: ExperimentFull,
+  analysesByStrategy: AnalysesByStrategy,
+): {
+  ratios: {
+    overall: {
+      exposedToAssigned: number
+      assignedSpammersToAssigned: number
+      assignedCrossoversToAssigned: number
+    }
+    byVariationId: Record<number, VariationRatios>
+  }
+  probabilities: {
+    byVariationId: Record<number, VariationProbabilites>
+  }
+} {
+  const participantCounts = getParticipantCounts(experiment, analysesByStrategy)
+
+  const ratios = {
+    overall: {
+      exposedToAssigned: participantCounts.total.exposed / participantCounts.total.assigned,
+      assignedSpammersToAssigned: participantCounts.total.assignedSpammers / participantCounts.total.assigned,
+      assignedCrossoversToAssigned: participantCounts.total.assignedCrossovers / participantCounts.total.assigned,
+    },
+    byVariationId: Object.fromEntries(
+      Object.entries(participantCounts.byVariationId).map(([variationId, variationCountsSet]) => {
+        return [
+          variationId,
+          {
+            exposedToAssigned: variationCountsSet.exposed / variationCountsSet.assigned,
+            assignedSpammersToAssigned: variationCountsSet.assignedSpammers / variationCountsSet.assigned,
+            assignedCrossoversToAssigned: variationCountsSet.assignedCrossovers / variationCountsSet.assigned,
+            exposedToTotalExposed: variationCountsSet.exposed / participantCounts.total.exposed,
+            assignedToTotalAssigned: variationCountsSet.assigned / participantCounts.total.assigned,
+            assignedSpammersToTotalAssignedSpammers:
+              variationCountsSet.assignedSpammers / participantCounts.total.assignedSpammers,
+            assignedCrossoversToTotalAssignedCrossovers:
+              variationCountsSet.assignedCrossovers / participantCounts.total.assignedCrossovers,
+          },
+        ]
+      }),
+    ),
+  }
+
+  const totalAllocatedPercentage = experiment.variations
+    .map(({ allocatedPercentage }) => allocatedPercentage)
+    .reduce((acc, cur) => acc + cur)
+  // The probability of an equal or a more extreme outcome occuring.
+  const probabilities = {
+    byVariationId: Object.fromEntries(
+      experiment.variations.map(({ variationId, allocatedPercentage }) => {
+        const variationCountsSet = participantCounts.byVariationId[variationId]
+        return [
+          variationId,
+          {
+            exposedDistributionMatchingAllocated: binomialTest(
+              variationCountsSet.exposed,
+              participantCounts.total.exposed,
+              { p: allocatedPercentage / totalAllocatedPercentage },
+            ).pValue,
+            assignedDistributionMatchingAllocated: binomialTest(
+              variationCountsSet.assigned,
+              participantCounts.total.assigned,
+              { p: allocatedPercentage / totalAllocatedPercentage },
+            ).pValue,
+            assignedSpammersDistributionMatchingAllocated: binomialTest(
+              variationCountsSet.assignedSpammers,
+              participantCounts.total.assignedSpammers,
+              { p: allocatedPercentage / totalAllocatedPercentage },
+            ).pValue,
+          },
+        ]
+      }),
+    ),
+  }
+
+  return {
+    ratios,
+    probabilities,
   }
 }
