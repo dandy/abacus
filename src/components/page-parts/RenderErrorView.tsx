a2881db4a117143d7531de870cafeb1c8b508db9
Jessie Ross <jessie.ross@automattic.com>
2021-05-07 05:35:25 +0800

    Improve error dialog (#519)
    * Improve error dialog
    
    * Add hippo
    
    * Minor fix
    
    * Fix snapshots
    
    * Update snapshots
    
    * Update snapshots back

--- src/components/page-parts/RenderErrorView.tsx
+++ src/components/page-parts/RenderErrorView.tsx
@@ -1,14 +1,57 @@
+import {
+  Button,
+  createStyles,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core'
 import React from 'react'
 
 import type { RenderError } from 'src/components/page-parts/RenderErrorBoundary'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+    pre: {
+      overflow: 'scroll',
+      maxHeight: '10rem',
+      background: '#f5f5f5',
+      padding: theme.spacing(2),
+    },
+    dialogContent: {
+      maxWidth: 562,
+    },
+  }),
+)
+
 export default function (props: { renderError: RenderError }): JSX.Element {
+  const classes = useStyles()
   return (
-    <>
-      <strong>Oops!</strong>
-      <p>{props.renderError.error.message}</p>
-      <p>An error occurred. If error persists, please contact the Experiments Platform team.</p>
-      <button onClick={props.renderError.clear}>Clear</button>
-    </>
+    <div className={classes.root}>
+      <Dialog open={true} maxWidth={false}>
+        <DialogTitle disableTypography>
+          <Typography variant='h5'>Oops! Something went wrong...</Typography>
+        </DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <img
+            src='/img/hippo-with-turtle.jpg'
+            alt={`"hippo" by .wilkie is licensed with CC BY-NC-SA 2.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/2.0/`}
+          />
+          <pre className={classes.pre}>{props.renderError.error.stack || props.renderError.error.message}</pre>
+        </DialogContent>
+        <DialogActions>
+          <Button component={'a'} href='/'>
+            Go home
+          </Button>
+          <Button onClick={props.renderError.clear} color='primary'>
+            Try again
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   )
 }
