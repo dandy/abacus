b66a3c75f632a2420162e1376311b8f97f63ba19
Jessie Ross <jessie.ross@automattic.com>
2021-05-07 16:36:23 +0800

    Style toolbar and controls

--- src/components/experiments/multi-view/ExperimentsTableAgGrid.tsx
+++ src/components/experiments/multi-view/ExperimentsTableAgGrid.tsx
@@ -2,7 +2,8 @@
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
 
-import { createStyles, Link, makeStyles, Theme, useTheme } from '@material-ui/core'
+import { Button, createStyles, fade, InputBase, Link, makeStyles, Theme, Typography, useTheme } from '@material-ui/core'
+import { Search as SearchIcon } from '@material-ui/icons'
 import { ColumnApi, GridApi, GridReadyEvent } from 'ag-grid-community'
 import { AgGridReact } from 'ag-grid-react'
 import clsx from 'clsx'
@@ -25,11 +26,58 @@ const statusComparator = (statusA: Status, statusB: Status) => {
   return statusOrder[statusA] - statusOrder[statusB]
 }
 
-const useStyles = makeStyles((_theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
+      flexDirection: 'column',
       flex: 1,
+    },
+    toolbar: {
+      margin: theme.spacing(4, 0),
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+    controls: {
+      display: 'flex',
+    },
+    gridContainer: {
+      display: 'flex',
+      flex: 1,
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.8),
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
     },
   }),
 )
@@ -58,97 +106,118 @@ const ExperimentsTable = ({ experiments }: { experiments: ExperimentBare[] }): J
 
   return (
     <div className={clsx('ag-theme-alpine', classes.root)}>
-      <AgGridReact
-        columnDefs={[
-          {
-            headerName: 'Name',
-            field: 'name',
-            cellStyle: {
-              fontFamily: theme.custom.fonts.monospace,
-              fontWeight: 600,
-            },
-            cellRendererFramework: ({ value: name, data }: { value: Status; data: ExperimentBare }) => (
-              <Link component={RouterLink} to={`/experiments/${createIdSlug(data.experimentId, data.name)}`}>
-                {name}
-              </Link>
-            ),
-            sortable: true,
-            filter: true,
-            resizable: true,
-            flex: 0,
-          },
-          {
-            headerName: 'Status',
-            field: 'status',
-            cellRendererFramework: ({ value: status }: { value: Status }) => <ExperimentStatus status={status} />,
-            comparator: statusComparator,
-            sortable: true,
-            filter: true,
-            resizable: true,
-          },
-          {
-            headerName: 'Platform',
-            field: 'platform',
-            cellStyle: {
-              fontFamily: theme.custom.fonts.monospace,
-            },
-            sortable: true,
-            filter: true,
-            resizable: true,
-          },
-          {
-            headerName: 'Owner',
-            field: 'ownerLogin',
-            cellStyle: {
-              fontFamily: theme.custom.fonts.monospace,
-            },
-            sortable: true,
-            filter: true,
-            resizable: true,
-          },
-          {
-            headerName: 'Start',
-            field: 'startDatetime',
-            cellRendererFramework: ({ value: startDatetime }: { value: Date }) => {
-              return <DatetimeText datetime={startDatetime} excludeTime />
-            },
-            sortable: true,
-            filter: 'agDateColumnFilter',
-            resizable: true,
-          },
-          {
-            headerName: 'End',
-            field: 'endDatetime',
-            cellRendererFramework: ({ value: endDatetime }: { value: Date }) => {
-              return <DatetimeText datetime={endDatetime} excludeTime />
-            },
-            sortable: true,
-            filter: 'agDateColumnFilter',
-            resizable: true,
-          },
-        ]}
-        rowData={experiments}
-        containerStyle={{ flex: 1, height: 'auto' }}
-        onFirstDataRendered={(event) => {
-          event.columnApi.autoSizeAllColumns()
-          event.columnApi.applyColumnState({
-            state: [
-              {
-                colId: 'status',
-                sort: 'asc',
-                sortIndex: 0,
+      <div className={classes.toolbar}>
+        <Typography variant='h2'>Experiments</Typography>
+        <div className={classes.controls}>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='Search…'
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <Button> Reset </Button>
+        </div>
+      </div>
+      <div className={clsx('ag-theme-alpine', classes.gridContainer)}>
+        <AgGridReact
+          columnDefs={[
+            {
+              headerName: 'Name',
+              field: 'name',
+              cellStyle: {
+                fontFamily: theme.custom.fonts.monospace,
+                fontWeight: 600,
               },
-              {
-                colId: 'startDatetime',
-                sort: 'desc',
-                sortIndex: 1,
+              cellRendererFramework: ({ value: name, data }: { value: Status; data: ExperimentBare }) => (
+                <Link component={RouterLink} to={`/experiments/${createIdSlug(data.experimentId, data.name)}`}>
+                  {name}
+                </Link>
+              ),
+              sortable: true,
+              filter: true,
+              resizable: true,
+              flex: 0,
+            },
+            {
+              headerName: 'Status',
+              field: 'status',
+              cellRendererFramework: ({ value: status }: { value: Status }) => <ExperimentStatus status={status} />,
+              comparator: statusComparator,
+              sortable: true,
+              filter: true,
+              resizable: true,
+            },
+            {
+              headerName: 'Platform',
+              field: 'platform',
+              cellStyle: {
+                fontFamily: theme.custom.fonts.monospace,
               },
-            ],
-            defaultState: { sort: null },
-          })
-        }}
-        onGridReady={onGridReady}
-      />
+              sortable: true,
+              filter: true,
+              resizable: true,
+            },
+            {
+              headerName: 'Owner',
+              field: 'ownerLogin',
+              cellStyle: {
+                fontFamily: theme.custom.fonts.monospace,
+              },
+              sortable: true,
+              filter: true,
+              resizable: true,
+            },
+            {
+              headerName: 'Start',
+              field: 'startDatetime',
+              cellRendererFramework: ({ value: startDatetime }: { value: Date }) => {
+                return <DatetimeText datetime={startDatetime} excludeTime />
+              },
+              sortable: true,
+              filter: 'agDateColumnFilter',
+              resizable: true,
+            },
+            {
+              headerName: 'End',
+              field: 'endDatetime',
+              cellRendererFramework: ({ value: endDatetime }: { value: Date }) => {
+                return <DatetimeText datetime={endDatetime} excludeTime />
+              },
+              sortable: true,
+              filter: 'agDateColumnFilter',
+              resizable: true,
+            },
+          ]}
+          rowData={experiments}
+          containerStyle={{ flex: 1, height: 'auto' }}
+          onFirstDataRendered={(event) => {
+            event.columnApi.autoSizeAllColumns()
+            event.columnApi.applyColumnState({
+              state: [
+                {
+                  colId: 'status',
+                  sort: 'asc',
+                  sortIndex: 0,
+                },
+                {
+                  colId: 'startDatetime',
+                  sort: 'desc',
+                  sortIndex: 1,
+                },
+              ],
+              defaultState: { sort: null },
+            })
+          }}
+          onGridReady={onGridReady}
+        />
+      </div>
     </div>
   )
 }
