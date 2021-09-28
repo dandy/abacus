9e3d70afda10b4b2d83019f3ed2333acb7de4fc7
Ijlal Hasnain <ijlal2@gmail.com>
2021-09-28 22:55:35 +0200

    Fix tests for new server
    Added same metric to be used in create, update, update. Also enabled a
    skipped test.

--- src/__tests__/api/MetricsApi.test.ts
+++ src/__tests__/api/MetricsApi.test.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 
 import HttpResponseError from 'src/api/HttpResponseError'
 import MetricsApi from 'src/api/MetricsApi'
-import { metricNewOutboundSchema, TransactionTypes } from 'src/lib/schemas'
+import { Metric, metricNewOutboundSchema, TransactionTypes } from 'src/lib/schemas'
 import Fixtures from 'src/test-helpers/fixtures'
 import { validationErrorDisplayer } from 'src/test-helpers/test-utils'
 
@@ -42,17 +42,21 @@ describe('MetricsApi.ts module', () => {
     })
   })
 
+  let metricInContext: Metric = {} as Metric;
   describe('create', () => {
     it(`should create a new metric`, async () => {
       const returnedMetric = await validationErrorDisplayer(MetricsApi.create(Fixtures.createMetric(1)))
+      metricInContext = returnedMetric;
       expect(returnedMetric.metricId).toBeGreaterThan(0)
     })
   })
 
   describe('put', () => {
     it(`should put a metric`, async () => {
-      const returnedMetric = await validationErrorDisplayer(MetricsApi.put(1, Fixtures.createMetric(1)))
-      expect(returnedMetric.metricId).toBeGreaterThan(0)
+      const new_metric_name = "change_name_of_metric";
+      const returnedMetric = await validationErrorDisplayer(MetricsApi.put(metricInContext.metricId, Fixtures.createMetric(metricInContext.metricId, { name: new_metric_name })))
+      expect(returnedMetric.metricId).toEqual(metricInContext.metricId);
+      expect(returnedMetric.name).toEqual(new_metric_name);
     })
   })
 
@@ -67,14 +71,14 @@ describe('MetricsApi.ts module', () => {
     it('should return the metric with the expected metric shape', async () => {
       // TODO: Test different metrics with different parameter types (conversion and
       // revenue). Can't do it now because only one metric is available to test.
-      const metric = await validationErrorDisplayer(MetricsApi.findById(31))
-      expect(metric.metricId).toBeGreaterThan(0)
+      const metric = await validationErrorDisplayer(MetricsApi.findById(metricInContext.metricId))
+      expect(metric.metricId).toEqual(metricInContext.metricId)
     })
 
     // TODO: Unskip this once the mock API stops returning the mock metric regardless
     // of the given ID. Also, remove the `instanbul ignore` comment from NotFoundError
     // and in `api/utils.ts` above the `if (response.status === 404)`.
-    it.skip('called with an unknown metric ID should throw a NotFoundError', async () => {
+    it('called with an unknown metric ID should throw a NotFoundError', async () => {
       try {
         await MetricsApi.findById(0)
         expect(false).toBe(true) // This should never be reached.
